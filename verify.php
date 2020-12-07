94fc44c540e47ef3ba3febe7a5108d8c86a71b95
tahsinAmin <muhammadtahsinamin@gmail.com>
2020-12-07 13:14:42 +0600

    Update password complete

--- verify.php
+++ verify.php
@@ -1,42 +1,43 @@
 <?php
 
-  include('config/db_connect.php');
+include('config/db_connect.php');
 
-  $last_name = $email = $password = '';
+$last_name = $password = $email = '';
 
-  $errors = array('last_name' => '', 'password' => '', 'email' => '');
+$errors = array('last_name' => '', 'password' => '', 'email' => '');
 
-	if(isset($_POST['submit'])){
+if(isset($_POST['submit'])){
 
-    // check last_name
-    if(empty($_POST['last_name'])){
-      $errors['last_name'] = 'Last Name is required';
-    } else{
-      $first_name = $_POST['last_name'];
-      if(!preg_match('/^[a-zA-Z\s]+$/', $last_name)){
-        $errors['last_name'] = 'Last Name must be letters and spaces only';
-      }
+  // check last_name
+  if(empty($_POST['last_name'])){
+    $errors['last_name'] = 'Last Name is required';
+  } else{
+    $last_name = $_POST['last_name'];
+    if(!preg_match('/^[a-zA-Z\s]+$/', $last_name)){
+      $errors['last_name'] = 'Last Name must be letters and spaces only';
     }
+  }
 
-    // check password
-    if(empty($_POST['password'])){
-      $errors['password'] = 'password is required';
-    } else{
-      $password = $_POST['password'];
-      if(!preg_match('/^[a-zA-Z0-9]+$/', $password)){
-        $errors['password'] = 'password must be letters and numbers only';
-      }
+  // check password
+  if(empty($_POST['password'])){
+    $errors['password'] = 'password is required';
+  } else{
+    $password = $_POST['password'];
+    if(!preg_match('/^[a-zA-Z0-9]+$/', $password)){
+      $errors['password'] = 'password must be letters and numbers only';
     }
+  }
 
-    // check email
-    if(empty($_POST['email'])){
-      $errors['email'] = 'An email is required';
-    } else{
-      $email = $_POST['email'];
-      if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-        $errors['email'] = 'Email must be a valid email address';
-      }
+  // check email
+  if(empty($_POST['email'])){
+    $errors['email'] = 'An email is required';
+  } else{
+    $email = $_POST['email'];
+    if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+      $errors['email'] = 'Email must be a valid email address';
     }
+  }
+
 
     if (array_filter($errors)) {
       // echo "errors in the form";
@@ -48,7 +49,7 @@
       $sql = "UPDATE buyer_profile SET password='".$password."' WHERE last_name='".$last_name."' AND email='".$email."'";
 
       if (mysqli_query($conn, $sql)) {
-        header("Location:checkout.php?buyer_id=".$buyer_id."");
+        header("Location:signin.php");
       }
     }
 	} // end POST check
@@ -60,22 +61,23 @@
 
 	<?php include('templates/header0.php'); ?>
 
-	<section class="container grey-text">
+  <section class="container grey-text">
 		<h4 class="center">Reset Password</h4>
-		<form class="white" action="signin.php" method="POST">
+		<form class="white" action="verify.php" method="POST">
       <label>Last Name</label>
       <input type="text" name="last_name" value="<?php echo htmlspecialchars($last_name) ?>">
+      <div class="red-text"><?php echo $errors['last_name']; ?></div>
       <label>Email</label>
-      <input type="text" name="last_name" value="<?php echo htmlspecialchars($email) ?>">
+      <input type="text" name="email" value="<?php echo htmlspecialchars($email) ?>">
+      <div class="red-text"><?php echo $errors['email']; ?></div>
       <label>Password</label>
       <input type="text" name="password" value="<?php echo htmlspecialchars($password) ?>">
       <div class="red-text"><?php echo $errors['password']; ?></div>
+
+
 			<div class="center">
 				<input type="submit" name="submit" value="Submit" class="btn brand z-depth-0">
 			</div>
-      <div class="center">
-        <a href="verify.php">Forgot Password?</a>
-      </div>
 		</form>
 	</section>
 
